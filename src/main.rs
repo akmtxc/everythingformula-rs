a15353bcedf97b65dcc8786851ed5f4fced751d1
akmtxc <akmtxc@disroot.org>
2021-01-08 19:57:25 +0500

    Fix ways

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use std::num::Wrapping;
 use num::{BigUint};
 
 fn main() {
-  let lumized = image::open("C:/Users/Стас/Desktop/everythingformula/images/smallspruce.png").unwrap().into_luma8(); // перевожу в чёрно-белую систему цветов
-  lumized.save("C:/Users/Стас/Desktop/everythingformula/images/lumasmallspruce.png").unwrap(); // сохраняю
-  let twodecstr = image::open("C:/Users/Стас/Desktop/everythingformula/images/lumasmallspruce.png").unwrap().into_bytes(); // перевожу в байты
+  let lumized = image::open("../images/smallspruce.png").unwrap().into_luma8(); // перевожу в чёрно-белую систему цветов
+  lumized.save("../images/lumasmallspruce.png").unwrap(); // сохраняю
+  let twodecstr = image::open("../images/lumasmallspruce.png").unwrap().into_bytes(); // перевожу в байты
 
   let mut rightbyte: Vec<u8> = Vec::new(); 
   for (i, &item) in twodecstr.iter().enumerate() {
